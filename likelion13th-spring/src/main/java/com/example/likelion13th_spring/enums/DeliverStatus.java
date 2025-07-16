d4ae9b27f5539c79e4bebe539d5f541598d28a1e
wontop02 <wjy0502kr@naver.com>
2025-07-16 20:56:05 +0900

    [FEAT] Add domain

---
+++ likelion13th-spring/src/main/java/com/example/likelion13th_spring/enums/DeliverStatus.java
@@ -0,0 +1,11 @@
+package com.example.likelion13th_spring.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum DeliverStatus {
+    PREPARATION, // 배송준비
+    DEPARTURE, // 배송출발
+    COMPLETED// 배송완료
+}
+
